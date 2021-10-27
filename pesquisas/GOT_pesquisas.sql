064c0d6c8acb914575b3e3ac5b35e7e00062131e
Hyan Araujo <88167146+B34tdb@users.noreply.github.com>
2021-10-26 22:14:52 -0300

    subindo as minhas perguntas

--- pesquisas/GOT_pesquisas.sql
+++ pesquisas/GOT_pesquisas.sql
@@ -1,6 +1,21 @@
+ Qual temporada teve a melhor/pior avalição
+
+SELECT 
+	temporada
+	, count(pontuacao) AS media
+FROM
+	episodios AS A
+GROUP BY
+	temporada
+ORDER BY 
+	media Desc limit 5;
+
+
+ Média de rating de cada ator Star_1 
 select 
-*
+estrela1,
+    titulo
 from 
-personagem 
-where 
-id >17
+episodios
+order by estrela1 desc limit 16
+
